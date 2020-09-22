2d6cb64efbe2e064be5eb5629525f31cc1b492a6
michinori-ozawa <70733365+michinori-ozawa@users.noreply.github.com>
2020-09-22 20:38:20 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
+
+        // 配列の各要素に値を代入する
+        array [0] = 00;
+        array [1] = 11;
+        array [2] = 22;
+        array [3] = 33;
+        array [4] = 44;
+
+
+
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        // 配列の要素をすべて表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
