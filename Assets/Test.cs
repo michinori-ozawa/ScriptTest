6cb772edb6e074860f91ff819946807d75529018
michinori-ozawa <70733365+michinori-ozawa@users.noreply.github.com>
2020-09-24 21:04:11 +0900

    Lesson4の課題の再々々々提出です。
    基本課題と応用課題の２つを一つのスクリプトに纏めました

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+
 public class Boss
 {
     private int hp = 100;          // 体力
@@ -33,10 +34,31 @@ public class Boss
         }
     }
 }
+
 public class Test : MonoBehaviour
 {
+    // Use this for initialization
     void Start()
     {
+        // 要素数5の配列を初期化する
+        int[] array = new int[5];
+        // 配列の各要素に値を代入する
+        array[0] = 00;
+        array[1] = 11;
+        array[2] = 22;
+        array[3] = 33;
+        array[4] = 44;
+        // 配列の要素をすべて表示する
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+        // 配列の要素をすべて表示する
+        for (int i = 4; i >= 0; i--)
+        {
+            Debug.Log(array[i]);
+        }
+ 
         // Bossクラスの変数を宣言してインスタンスを代入
         Boss lastboss = new Boss();
         // 攻撃用の関数を呼び出す
